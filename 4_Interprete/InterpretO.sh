8b47b708f7f2784729c1537da91de50294866974
FabianColque <fabian.colque@hotmail.com>
2018-05-19 19:22:05 -0300

    Agrego fucion para procesar archivos en Aceptados y primera lectura de archivos con tabla T1  (falta refactorizar)

--- 4_Interprete/InterpretO.sh
+++ 4_Interprete/InterpretO.sh
@@ -1,47 +1,61 @@
 grupo=$HOME/Grupo4
-fnoc=$grupo/dirconf/fnoc.conf
+
+#Estas variables deberian venir seteadas desde InicializO.sh
 directorioAceptados=$HOME/Grupo4/acep
-
-	###Busco directorio de maestros en el archivo de configuracion
-	# lineaMaestros=$(grep '^Maestros-' $fnoc)
-	# directorioMaestros=$(echo $lineaMaestros | cut -d '-' -f 2)
-	# archivoT1=$grupo/$directorioMaestros/T1.tab
-
-
-
-	###Busco todos los archivos a procesar
-	# cd $grupo/acep
-	# archivosAProcesar=$(ls)
-
-
-	###Por cada archivo a procesar, busco sus delimitadores
-	# for archivo in $archivosAProcesar
-	# do
-	#  echo "FILE: $archivo"
-	#  pais=$(echo $archivo | cut -c 1)
-	#  echo "Pais: $pais"
-	#  sistema=$(echo $archivo | cut -c 3)
-	#  echo "Sistema: $sistema"
-	#
-	#  regex=$(grep $pais-$sistema $archivoT1)
-	# echo "regex $regex"
-	#  delimitador_campos=$(echo $regex | cut -c 5)
-	#  echo "Delimitador campos: $delimitador_campos"
-	#  delimitador_decimal=$(echo $regex | cut -c 7)
-	#  echo "Delimitador decimal: $delimitador_decimal"
-	# done
+directorioArchivosMaestros=$HOME/Grupo4/mae
+directorioProcesados=$HOME/Grupo4/proc
 
 Procesar()
 {
 	#Fijate que a directorioAceptados ponele el nombre que vos le pusiste al crearlo
 	#Esa variable nos la tiene que dar InicializO.sh
-	if [ -e $grupo/$directorioAceptados ]
+	if [ -e $directorioAceptados ]
 		then
 			echo "Si existen archivos a procesar"
 			#Procesar cada archivo
+			EvaluarArchivos
 	else
 		echo "No hay archivos para ser procesados."
 	fi
+}
+
+EvaluarArchivos()
+{
+	###Busco archivo T1
+	echo "evaluacion"
+	archivoT1=$directorioArchivosMaestros/T1.tab
+
+	#Busco todos los archivos a procesar
+	 cd
+	 cd $directorioAceptados
+	archivosAProcesar=$(ls)
+
+	# while ( cd $directorioAceptados && ls -1 )
+	# do
+	# 	files=( directorioAceptados/* )
+	# 	echo "${files[0]}"
+	# 	#archivoAMover= ( cd $directorioAceptados | ls | head -1 )
+	#
+	# 	#archivoAMover="$directorioAceptados/$archivo"
+	# 	mv ${files[0]} $directorioProcesados
+	#
+	# done
+
+	for archivo in $archivosAProcesar
+	do
+	 echo "FILE: $archivo"
+	 pais=$(echo $archivo | cut -c 1)
+	 echo "Pais: $pais"
+	 sistema=$(echo $archivo | cut -c 3)
+	 echo "Sistema: $sistema"
+
+	 regex=$(grep $pais-$sistema $archivoT1)
+	echo "regex $regex"
+	 delimitador_campos=$(echo $regex | cut -c 5)
+	 echo "Delimitador campos: $delimitador_campos"
+	 delimitador_decimal=$(echo $regex | cut -c 7)
+	 echo "Delimitador decimal: $delimitador_decimal"
+	done
 }
 
 VerificarEstadoInicializacion()
