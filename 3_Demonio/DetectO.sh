754a98d14d823ecc76386b9479e63186550aa6ed
javier <javier.nunezleyes@gmail.com>
2018-05-20 18:31:25 -0300

    Agrego resolucion de colisiones en archivos

--- 3_Demonio/DetectO.sh
+++ 3_Demonio/DetectO.sh
@@ -1,18 +1,30 @@
+#!/bin/bash
+
 # Se ejecuta en background . ./DetectO.sh &
-# Cambiar llamadas al logger
-# Cambiar $INIT_OK
-# Cambiar $DIRECTORIO_ARRIBOS
-# Cambiar $DIRECTORIO_ACEPTADOS
-# Cambiar $DIRECTORIO_RECHAZADOS
+
+# Cambiar llamadas al logger [LOG]
+# Poner mensajes en [LOG]
+# Nombre DetecO.log
+
+# Cambiar variables de ambiten [REPLACE]
+
+# Falta verificar periodo actual
+# Falta verificar con los archivos maestros
 
 ######################################################### Funciones auxiliares #########################################################
+
+# grep -c '^[Aa-Zz]\{1\}-[0-9]\{1\}-[0-9]\{4\}-[0-9]\{2\}\.txt$'
+# grep -c -v '^$'
+# ls | grep -c -i A-8-2018-04.txt
+# ls | grep -c duplicados
+# date +%Y-%m-%d_%H:%M:%S
 
 Invocar_interprete()
 {
 	# Invocar al interprete si no se encuentra en ejecucion
 	# registro que sucedio con la invocacion [LOG]
 
-	echo "debug:"	
+	echo "debug: Invocando al interprete"	
 }
 
 Mover_archivos_rechazados()
@@ -23,21 +35,56 @@ Mover_archivos_rechazados()
 	for archivo_rechazado in $(cd $DIRECTORIO_ARRIBOS && ls)
 	do
 		# [DEBUG]: Borrar
-	    echo "Moviendo archivo: $archivo_rechazado"
+	    # echo "Moviendo archivo: $archivo_rechazado"
 
-	    mv $DIRECTORIO_ARRIBOS/$archivo_rechazado $DIRECTORIO_RECHAZADOS/$archivo_rechazado
+	    # [LOG] archivo rechazado: $archivo_rechazado
+
+	    Mover_archivo $DIRECTORIO_ARRIBOS $DIRECTORIO_RECHAZADOS $archivo_rechazado
 
 	done 
 }
 
-Mover_a_carpeta_aceptados()
+# Mover_archivo() DIRECTORIO_ORIGEN DIRECTORIO_DESTINO NOMBRE_ARCHIVO
+Mover_archivo()
 {
 	# [DEBUG]
-	echo "debug: Moviendo archivo aceptado: $1"
+	echo "debug: Moviendo archivo $3"
 
-	# [LOG] archivo aceptado: $1
+	esta_duplicado=$(ls $2 | grep -c -i "$3")
 
-	mv $DIRECTORIO_ARRIBOS/$1 $DIRECTORIO_ACEPTADOS/$1
+	# [DEBUG]
+	# echo "Esta duplicado: $esta_duplicado"
+
+	existe_carpeta_duplicados=$(ls $2 | grep -c duplicados)
+
+	# [DEBUG]
+	# echo "Existe carpeta duplicados: $existe_carpeta_duplicados"
+
+	if [ $esta_duplicado == 1 ]
+	then
+
+		if [ $existe_carpeta_duplicados == 0 ]
+		then
+			# [LOG] se crea carpeta de duplicados
+			mkdir $2/duplicados
+
+			# [DEBUG]
+			echo "Creo carpeta duplicados"
+		fi
+
+		fecha_duplicado=$(date +%Y-%m-%d_%H:%M:%S)
+
+		# [DEBUG]
+		# echo "Fecha archivo duplicado: $fecha_duplicado"
+
+		mv $1/$3 $2/duplicados/$3_$fecha_duplicado
+		# [LOG] archivo aceptado duplicado: $3_$fecha_duplicado
+
+	else
+
+		# [LOG] archivo aceptado: $1
+		mv $1/$3 $2/$3
+	fi
 }
 
 Verificar_archivo_recibido()
@@ -89,7 +136,6 @@ DIRECTORIO_ARRIBOS=/home/jleyes/ARRIBOS
 DIRECTORIO_ACEPTADOS=/home/jleyes/Aceptados
 DIRECTORIO_RECHAZADOS=/home/jleyes/Rechazados
 
-# Cuento los ciclos
 numero_ciclo=1
 
 while true
@@ -100,7 +146,7 @@ do
 	for nombre_archivo in $(cd $DIRECTORIO_ARRIBOS && ls *.txt)
 	do
 		# [DEBUG]: Borrar
-	    echo $nombre_archivo
+	    # echo $nombre_archivo
 
 	    # Analizo el archivo
 	    Verificar_archivo_recibido $nombre_archivo	
@@ -111,9 +157,9 @@ do
     	if [ $archivo_valido == 1 ]
 		then
 			# [DEBUG]: Borrar
-    		echo "Es valido: $archivo_valido"
+    		# echo "Es valido: $archivo_valido"
 
-			Mover_a_carpeta_aceptados $nombre_archivo
+			Mover_archivo $DIRECTORIO_ARRIBOS $DIRECTORIO_ACEPTADOS $nombre_archivo
     	fi
 
 	done  
@@ -128,8 +174,3 @@ do
 	sleep 60
 
 done
-
-# Falta verificar periodo actual
-# Falta verificar con los archivos maestros
-# grep -c '^[Aa-Zz]\{1\}-[0-9]\{1\}-[0-9]\{4\}-[0-9]\{2\}\.txt$'
-# grep -c -v '^$'
