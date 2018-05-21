35555d835b321a14e33ce5169fd0dd83d8c625ac
javier <javier.nunezleyes@gmail.com>
2018-05-21 01:44:24 -0300

    Agrego invocacion al interprete

--- 3_Demonio/DetectO.sh
+++ 3_Demonio/DetectO.sh
@@ -6,7 +6,7 @@
 # Poner mensajes en [LOG]
 # Nombre DetecO.log
 
-# Cambiar variables de ambiten [REPLACE]
+# Cambiar variables de ambiente [REPLACE]
 
 # Falta verificar periodo actual
 # Falta verificar con los archivos maestros
@@ -17,14 +17,64 @@
 # grep -c -v '^$'
 # ls | grep -c -i A-8-2018-04
 # ls | grep -c duplicados
+# ls | wc -l
+# ps -p 1 | wc -l
 # date +%Y-%m-%d_%H:%M:%S
 
-Invocar_interprete()
+Iniciar_interprete()
 {
-	# Invocar al interprete si no se encuentra en ejecucion
-	# registro que sucedio con la invocacion [LOG]
+	# [REPLACE] InterpreteO
+	/home/jleyes/InterpreteMock.sh &
 
-	echo "debug: Invocando al interprete"	
+	# $! guarda el PID del proceso en background
+	pid_Interprete=$!	
+}
+
+# Invocar al interprete si hay archivos en la carpeta aceptados y si no se encuentra en ejecucion
+Validar_estado_interprete()
+{
+	# [DEBUG]
+	echo "debug: analizando estado del interprete"	
+
+	cantidad_archivos_aceptados=$(ls $DIRECTORIO_ACEPTADOS | wc -l)
+
+	if [ $cantidad_archivos_aceptados != 0 ]
+	then
+
+		# [LOG]
+		# [DEBUG]
+		echo "Hay archivos para ejecutar el interprete"
+
+		if [ $interprete_iniciado == false ]
+		then
+			Iniciar_interprete		
+
+			interprete_iniciado=true
+
+			# [DEBUG]
+			echo "PID: $pid_Interprete"
+		else
+			interprete_sigue_corriendo=$(ps -p $pid_Interprete | wc -l)	
+
+			# interprete_sigue_corriendo == 1, no se encuentra en ejecucion
+			if [ $interprete_sigue_corriendo == 1 ]
+			then
+				Iniciar_interprete
+
+				# [LOG]
+				echo "PID: $pid_Interprete"
+			else
+				# [LOG]
+				echo "El interprete se esta ejecutando, queda para el siguiente ciclo"
+			fi
+		fi
+
+		return 
+	fi
+
+	# [LOG]
+	# [DEBUG]
+	echo "No hay archivos para ejecutar el interprete"		
 }
 
 Mover_archivos_rechazados()
@@ -65,6 +115,7 @@ Mover_archivo()
 
 		if [ $existe_carpeta_duplicados == 0 ]
 		then
+
 			# [LOG] se crea carpeta de duplicados
 			mkdir $2/duplicados
 
@@ -99,19 +150,20 @@ Verificar_archivo_recibido()
 	if [ $nombre_valido != 1 ] || [ $esta_vacio == 1 ] || [ ! -f $DIRECTORIO_ARRIBOS/$1 ]
 	then
 		# [DEBUG]
-		echo "Archivo invalido"
+		#echo "Archivo invalido"
+
 		return 0
 	fi
 
 	# [DEBUG]
-	echo "Archivo valido"
+	# echo "Archivo valido"
 
 	return 1
 }
 
 Verifico_inicializacion()
 {
-	if [ ! $exINIT_OK ]
+	if [ $INIT_OK == false ]
 	then
 		# [LOG]: El sistema no esta correctamente inicializado
 		echo "El sistema no esta correctamente inicializado."
@@ -125,19 +177,25 @@ Verifico_inicializacion()
 
 ######################################################### Detector de novedades #########################################################
 
+# [REPLACE]: !REEMPLAZAR POR VARIABLE DE INICIALIZADOR!
+INIT_OK=true
+
 Verifico_inicializacion
 
 # Seteo variables de ambiente
-grupo=$HOME/Grupo4
-DIRECTORIO_ARRIBOS=$grupo/$exDIR_EXT
-DIRECTORIO_ACEPTADOS=$grupo/$exDIR_ACCEPT
-DIRECTORIO_RECHAZADOS=$grupo/$exDIR_REFUSE
+# [REPLACE]: !REEMPLAZAR POR VARIABLE DE INICIALIZADOR!
+DIRECTORIO_ARRIBOS=/home/jleyes/ARRIBOS
+DIRECTORIO_ACEPTADOS=/home/jleyes/Aceptados
+DIRECTORIO_RECHAZADOS=/home/jleyes/Rechazados
 
 numero_ciclo=1
+
+interprete_iniciado=false
 
 while true
 do
 	# registro el numero de ciclo [LOG]
+	echo "Analizo directorio de archivos recibidos"
 
 	# Recorro los nombres de los archivos en el directorio de arribos
 	for nombre_archivo in $(cd $DIRECTORIO_ARRIBOS && ls)
@@ -145,7 +203,6 @@ do
 		# [DEBUG]: Borrar
 	    # echo $nombre_archivo
 
-	    # Analizo el archivo
 	    Verificar_archivo_recibido $nombre_archivo	
 
 	    # Guardo el resultado del analisis del archivo
@@ -164,7 +221,7 @@ do
 	# Muevo todos los archivos que no fueron aceptados
 	Mover_archivos_rechazados
 	
-	Invocar_interprete
+	Validar_estado_interprete
 
 	numero_ciclo=$(($numero_ciclo + 1))
 
