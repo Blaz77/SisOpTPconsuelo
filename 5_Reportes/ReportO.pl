5a7d1f88d61f04f3b65e18bd61b75632d5f414de
Blaz77 <lugusor@gmail.com>
2018-05-21 22:12:24 -0300

    ReportO

--- 5_Reportes/ReportO.pl
+++ 5_Reportes/ReportO.pl
@@ -1,7 +1,9 @@
 #!/usr/bin/perl
 
-print "Bienvenido al sistema de reportes Elija el tipo de reporte que desea realizar:";
-print "(1) - Recomendacion.";
-print "(2) - Listar Divergencias en porcentaje."
-print "(3) - Listar Divergencias en monto."
+system("clear");
+
+print "Bienvenido al sistema de reportes Elija el tipo de reporte que desea realizar:\n";
+print "(1) - Recomendacion.\n";
+print "(2) - Listar Divergencias en porcentaje.\n";
+print "(3) - Listar Divergencias en monto.\n";
 
