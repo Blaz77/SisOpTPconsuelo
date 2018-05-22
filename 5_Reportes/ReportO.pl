c04d9fa5656dfd3a5a618ea47ffa55321cf642c2
Blaz77 <lugusor@gmail.com>
2018-05-21 22:27:52 -0300

    ReportO

--- 5_Reportes/ReportO.pl
+++ 5_Reportes/ReportO.pl
@@ -2,6 +2,9 @@
 
 system("clear");
 
+my $ambienteOK = $ENV{exINIT_OK}
+die "El ambiente no esta inicializado. No se puede continuar." if $ambienteOK != 1;
+
 print "Bienvenido al sistema de reportes Elija el tipo de reporte que desea realizar:\n";
 print "(1) - Recomendacion.\n";
 print "(2) - Listar Divergencias en porcentaje.\n";
