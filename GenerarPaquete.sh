4262811d22575cb29fb40acac7dce2fd7b45bdd4
Blaz77 <lugusor@gmail.com>
2018-05-23 23:19:29 -0300

    Fix GenerarPaquete. Ticket #249, por jleyes

--- GenerarPaquete.sh
+++ GenerarPaquete.sh
@@ -19,35 +19,35 @@ echo
 
 echo "Eliminando paquete anterior..."
 
-rm -rf $DIFERENCIAL/$DIR_PAQUETE
+rm -rf "$DIFERENCIAL/$DIR_PAQUETE"
 
 echo "Creando directorios..."
 
-mkdir $DIFERENCIAL/$DIR_PAQUETE
-mkdir $DIFERENCIAL/$DIR_PAQUETE/Grupo4
-mkdir $DIFERENCIAL/$DIR_PAQUETE/Grupo4/dirconf
-mkdir $DIFERENCIAL/$DIR_PAQUETE/Grupo4/package
-mkdir $DIFERENCIAL/$DIR_PAQUETE/Grupo4/package/archivostp
-mkdir $DIFERENCIAL/$DIR_PAQUETE/Grupo4/package/scripts
+mkdir "$DIFERENCIAL/$DIR_PAQUETE"
+mkdir "$DIFERENCIAL/$DIR_PAQUETE/Grupo4"
+mkdir "$DIFERENCIAL/$DIR_PAQUETE/Grupo4/dirconf"
+mkdir "$DIFERENCIAL/$DIR_PAQUETE/Grupo4/package"
+mkdir "$DIFERENCIAL/$DIR_PAQUETE/Grupo4/package/archivostp"
+mkdir "$DIFERENCIAL/$DIR_PAQUETE/Grupo4/package/scripts"
 
 echo "Copiando archivos..."
 
-cp $DIFERENCIAL/$DIR_METADATOS/* $DIFERENCIAL/$DIR_PAQUETE/Grupo4/package/archivostp
-cp $DIFERENCIAL/$DIR_DATOSPRUEBA/* $DIFERENCIAL/$DIR_PAQUETE/Grupo4/package/archivostp
-cp $DIFERENCIAL/"1_Instalador"/"InstalO.sh" $DIFERENCIAL/$DIR_PAQUETE/Grupo4
-cp $DIFERENCIAL/"2_Inicializador"/"IniciO.sh" $DIFERENCIAL/$DIR_PAQUETE/Grupo4/package/scripts
-cp $DIFERENCIAL/"2_Inicializador"/"StopO.sh" $DIFERENCIAL/$DIR_PAQUETE/Grupo4/package/scripts
-cp $DIFERENCIAL/"3_Demonio"/"DetectO.sh" $DIFERENCIAL/$DIR_PAQUETE/Grupo4/package/scripts
-cp $DIFERENCIAL/"4_Interprete"/"InterpretO.sh" $DIFERENCIAL/$DIR_PAQUETE/Grupo4/package/scripts
-cp $DIFERENCIAL/"5_Reportes"/"ReportO.pl" $DIFERENCIAL/$DIR_PAQUETE/Grupo4/package/scripts
-cp $DIFERENCIAL/"Logger.sh" $DIFERENCIAL/$DIR_PAQUETE/Grupo4/package/scripts
+cp "$DIFERENCIAL/$DIR_METADATOS/"* "$DIFERENCIAL/$DIR_PAQUETE/Grupo4/package/archivostp"
+cp "$DIFERENCIAL/$DIR_DATOSPRUEBA/"* "$DIFERENCIAL/$DIR_PAQUETE/Grupo4/package/archivostp"
+cp "$DIFERENCIAL/1_Instalador/InstalO.sh" "$DIFERENCIAL/$DIR_PAQUETE/Grupo4"
+cp "$DIFERENCIAL/2_Inicializador/IniciO.sh" "$DIFERENCIAL/$DIR_PAQUETE/Grupo4/package/scripts"
+cp "$DIFERENCIAL/2_Inicializador/StopO.sh" "$DIFERENCIAL/$DIR_PAQUETE/Grupo4/package/scripts"
+cp "$DIFERENCIAL/3_Demonio/DetectO.sh" "$DIFERENCIAL/$DIR_PAQUETE/Grupo4/package/scripts"
+cp "$DIFERENCIAL/4_Interprete/InterpretO.sh" "$DIFERENCIAL/$DIR_PAQUETE/Grupo4/package/scripts"
+cp "$DIFERENCIAL/5_Reportes/ReportO.pl" "$DIFERENCIAL/$DIR_PAQUETE/Grupo4/package/scripts"
+cp "$DIFERENCIAL/Logger.sh" "$DIFERENCIAL/$DIR_PAQUETE/Grupo4/package/scripts"
 
 echo "Generando tgz..."
 
-PWDBack=$PWD
-cd $DIFERENCIAL/$DIR_PAQUETE
-tar cvfz $PWDBack/$DIFERENCIAL/"Paquete.tgz" * > /dev/null
-cd $PWDBack
+PWDBack="$PWD"
+cd "$DIFERENCIAL/$DIR_PAQUETE"
+tar cvfz "$PWDBack/$DIFERENCIAL/Paquete.tgz" * > /dev/null
+cd "$PWDBack"
 
 echo "Listo."
 read -en 1
