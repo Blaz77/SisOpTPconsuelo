09b31624929e201f4404fffe4b5be6fd77a446b1
Eunice <euni.lewczuk@hotmail.com>
2018-06-18 23:38:23 -0300

    Intento de avanzar algo más: buscar el registro a comparar y recomendacion

--- 5_Reportes/ReportO2.pl
+++ 5_Reportes/ReportO2.pl
@@ -17,6 +17,8 @@ $PATH_MAESTRO = $NAME_FILE_MAESTRO;
 
 %MONTO_RESTANTE_PPI; #Este hash tiene como clave: codigo de prestamo, y valor: monto restante calculado
 
+%LINEAS_PRESTAMOS; #Hash que contiene las líneas del archivo préstamos que coinciden con préstamo, año y mes del hash de maestros %CONTABLE_PPI
+
 my %SISTEMAS;
 my $DIR_REPORTES;
 my $DIR_LOGS;
@@ -60,6 +62,11 @@ sub CalcularMontoRestante
 		@campos = split(";", $CONTABLE_PPI{"$pres_id"});
 		$anio_contable = $campos[2];
 		$mes_contable = $campos[3];
+		if ($mes_contable < 10)
+		{
+			#A los meses por ejemplo "5" los cambio por "05" para poder comparar con el otro archivo
+			$mes_contable = "0".$mes_contable;
+		}
 		$monto_prestamo = $campos[9];
 		$monto_impago = $campos[10];
 		$monto_devengado = $campos[11];
@@ -70,6 +77,8 @@ sub CalcularMontoRestante
 		$MONTO_RESTANTE_PPI{"$pres_id"} = $monto_restante;
 		#print "Montos: $monto_prestamo + $monto_impago + $monto_devengado + $monto_no_devengado - $monto_debitado";
 		#print "Restante: $monto_restante\n";
+		#$valor = $CONTABLE_PPI{"$pres_id"};
+		#print "Valor maestro $valor\n";
 	
 		open(PRESTAMOS, "<PRESTAMOS.Argentina") || die "ERROR: no se pudo abrir el archivo PRESTAMOS.Argentina";
 		while ($linea = <PRESTAMOS>)
@@ -77,15 +86,96 @@ sub CalcularMontoRestante
 			@campos_prestamos = split(";", $linea);
 			$anio_contable_prestamos = $campos_prestamos[1];
 			$mes_contable_prestamos = $campos_prestamos[2];
+			$dia_contable_prestamos = $campos_prestamos[3];
 			$pres_id_prestamos = $campos_prestamos[5];
+			$fecha_grabacion = $campos_prestamos[14];
 			$monto_restante_prestamos = $campos_prestamos[11];
 			$monto_restante_prestamos =~ s/,/./;
+			if ($pres_id eq $pres_id_prestamos and $anio_contable eq $anio_contable_prestamos and $mes_contable eq $mes_contable_prestamos)
+			{
+				$LINEAS_PRESTAMOS{"$pres_id_prestamos"} = "$linea";
+			}
 			#print "Monto restante préstamos: $monto_restante_prestamos\n";
 			#print "$anio_contable_prestamos, $mes_contable_prestamos, $pres_id_prestamos, $monto_restante_prestamos\n";
 		}
 		close(PRESTAMOS);
 	}
+}
 
+sub Comparar
+{
+	@values_prestamos = values(%LINEAS_PRESTAMOS);
+	$long = @values_prestamos;
+	#print "$long\n";
+	if ($long == 0)
+	{
+		print "No se encontró registro que coincida para comparar.\n";
+	}
+	elsif ($long > 1)
+	{
+		$dia_max = 0;
+		#print "Antes de eliminar:\n";
+		foreach $linea (@values_prestamos)
+		{
+			@campos_prestamos = split(";", $linea);
+			$dia_contable_prestamos = $campos_prestamos[3];
+			if ($dia_contable_prestamos > $dia_max)
+			{
+				$dia_max = $dia_contable_prestamos;
+			}
+			#print "$linea\n";
+		}
+		foreach $clave (keys(%LINEAS_PRESTAMOS))
+		{
+			$valor = $LINEAS_PRESTAMOS{"$clave"};
+			@campos_prestamos = split(";", $valor);
+			$dia_contable_prestamos = $campos_prestamos[3];
+			#print "dia contable prestamos $dia_contable_prestamos, dia max $dia_max\n";
+			if ($dia_contable_prestamos < $dia_max)
+			{
+				#print "elimino\n";
+				delete($LINEAS_PRESTAMOS{"$clave"});
+			}
+		}
+		#print "Despues de eliminar:\n";
+		foreach $clave (keys(%LINEAS_PRESTAMOS))
+		{
+			$valor = $LINEAS_PRESTAMOS{"$clave"};
+			#print "$valor\n";
+		}
+	}
+	#Falta la parte de "SI AÚN ASI hay más de un registro contra el cual comparar, tomar el de fecha de grabación más reciente"
+}
+
+sub Recomendar
+{
+	foreach $pres_id (keys(%CONTABLE_PPI))
+	{
+		#print "pres id $pres_id\n";
+		$linea_ppi = $CONTABLE_PPI{"$pres_id"};
+		@campos_ppi = split(";", $linea_ppi);
+		$estado_ppi = $campos_ppi[5];
+		$monto_rest_ppi = $MONTO_RESTANTE_PPI{"$pres_id"};
+
+		$linea_prestamos = $LINEAS_PRESTAMOS{"$pres_id"};
+		#print "linea prestamos $linea_prestamos\n";
+		@campos_prestamos = split(";", $linea_prestamos);
+		$estado_prestamos = $campos_prestamos[4];
+		$monto_restante_prestamos = $campos_prestamos[11];
+		#print "estado ppi $estado_ppi, estado prestamos $estado_prestamos\n";
+		#print "monto ppi $monto_rest_ppi, monto prestamos $monto_restante_prestamos\n";
+		if ($linea_prestamos ne "")
+		{
+			if ($estado_ppi eq "SMOR" and $estado_prestamos ne "SMOR")
+			{
+				#print "RECAL1\n";
+			}
+			if ($monto_rest_ppi < $monto_restante_prestamos)
+			{
+				#print "RECAL2\n";
+			}
+		}
+	}
 }
 
 sub Recomendacion_Cod_Pais
@@ -126,6 +216,8 @@ sub Recomendacion_Cod_Pais
 	close (Handler_maestro);
 
 	&CalcularMontoRestante;
+	&Comparar;
+	&Recomendar;
 }
 
 sub RealizarRecomendacion2
